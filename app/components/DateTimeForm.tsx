04e9cc1dcb3320eba05a4d727e58d88bd3d891d6
LizzieDworsky <amydworsky15@gmail.com>
2024-05-03 11:36:49 -0600

    updated DateTimeForm with date calendar portion

--- app/components/DateTimeForm.tsx
+++ app/components/DateTimeForm.tsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
 const DateTimeForm = ({}) => {
-    return <div>DateTimeForm</div>;
+    const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+    const options: Intl.DateTimeFormatOptions = {
+        weekday: "short",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+
+    const handleDateChange = (date: any) => {
+        if (date instanceof Date) {
+            setSelectedDate(date);
+        }
+    };
+
+    return (
+        <div>
+            <h3>Pick a Date and Time</h3>
+            <Calendar
+                selectRange={false}
+                onChange={(date) => handleDateChange(date)}
+                value={selectedDate}
+            />
+            {selectedDate && (
+                <h3>
+                    Available Starting times for{" "}
+                    {selectedDate.toLocaleDateString("en-US", options)}
+                </h3>
+            )}
+        </div>
+    );
 };
 
 export default DateTimeForm;
