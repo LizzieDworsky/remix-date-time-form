0547646924e87a93f858d37ce7aa9bdf29c3c542
LizzieDworsky <amydworsky15@gmail.com>
2024-05-03 15:17:22 -0600

    WIP implmenting time slots for appointments

--- app/components/DateTimeForm.tsx
+++ app/components/DateTimeForm.tsx
@@ -8,18 +8,55 @@ const DateTimeForm = ({}) => {
     const [selectedTimeZone, setSelectedTimeZone] = useState<string>(
         moment.tz.guess()
     );
+    const [selectedTimeSlot, setSelectedTimeSlot] = useState<string | null>(
+        null
+    );
+    const [isInitialSlot, setIsInitialSlot] = useState<boolean>(true);
 
     const handleDateChange = (date: any) => {
         if (date instanceof Date) {
             setSelectedDate(date);
+            setSelectedTimeSlot(null);
         }
     };
 
     const handleTimeZoneChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setSelectedTimeZone(e.target.value);
     };
-
     const timeZones = moment.tz.names();
+
+    const handleTimeSlotSelection = (timeSlot: string) => {
+        setSelectedTimeSlot(timeSlot);
+    };
+    const togglePMView = () => {
+        setIsInitialSlot(!isInitialSlot);
+    };
+
+    const timeSlotsAM = [
+        "09:00 AM",
+        "09:30 AM",
+        "10:00 AM",
+        "10:30 AM",
+        "11:00 AM",
+        "11:30 AM",
+    ];
+    const timeSlotsPMInitial = [
+        "12:00 PM",
+        "12:30 PM",
+        "01:00 PM",
+        "01:30 PM",
+        "02:00 PM",
+        "02:30 PM",
+    ];
+    const timeSlotsPMTwo = [
+        "02:00 PM",
+        "02:30 PM",
+        "03:00 PM",
+        "03:30 PM",
+        "04:00 PM",
+        "04:30 PM",
+    ];
+    const timeSlotsPM = isInitialSlot ? timeSlotsPMInitial : timeSlotsPMTwo;
 
     return (
         <div>
@@ -49,6 +86,33 @@ const DateTimeForm = ({}) => {
                         .format("ddd, MMMM D, YYYY")}
                 </h3>
             )}
+            <div>
+                <h4>AM</h4>
+                {timeSlotsAM.map((slot) => (
+                    <button
+                        key={slot}
+                        onClick={() => handleTimeSlotSelection(slot)}
+                    >
+                        {slot}
+                    </button>
+                ))}
+                <a href="#">
+                    <p>Load More</p>
+                </a>
+                <h4>PM</h4>
+                {timeSlotsPM.map((slot) => (
+                    <button
+                        key={slot}
+                        onClick={() => handleTimeSlotSelection(slot)}
+                    >
+                        {slot}
+                    </button>
+                ))}
+                <a href="#" onClick={togglePMView}>
+                    <p>Load More</p>
+                </a>
+            </div>
+            <button>Select Date</button>
         </div>
     );
 };
