b34b6437903958467b7c48176a9ff56c62cf9f08
LizzieDworsky <amydworsky15@gmail.com>
2024-05-03 14:44:02 -0600

    implemented timezone selection using moment.js

--- app/components/DateTimeForm.tsx
+++ app/components/DateTimeForm.tsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
+import moment from "moment-timezone";
 import "react-calendar/dist/Calendar.css";
 
 const DateTimeForm = ({}) => {
     const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
-    const options: Intl.DateTimeFormatOptions = {
-        weekday: "short",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-    };
+    const [selectedTimeZone, setSelectedTimeZone] = useState<string>(
+        moment.tz.guess()
+    );
 
     const handleDateChange = (date: any) => {
         if (date instanceof Date) {
             setSelectedDate(date);
         }
     };
+
+    const handleTimeZoneChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedTimeZone(e.target.value);
+    };
+
+    const timeZones = moment.tz.names();
 
     return (
         <div>
@@ -24,11 +28,25 @@ const DateTimeForm = ({}) => {
                 selectRange={false}
                 onChange={(date) => handleDateChange(date)}
                 value={selectedDate}
+                calendarType="gregory"
             />
+            <select
+                id="timezone"
+                value={selectedTimeZone}
+                onChange={handleTimeZoneChange}
+            >
+                {timeZones.map((zone) => (
+                    <option key={zone} value={zone}>
+                        {zone}
+                    </option>
+                ))}
+            </select>
             {selectedDate && (
                 <h3>
                     Available Starting times for{" "}
-                    {selectedDate.toLocaleDateString("en-US", options)}
+                    {moment(selectedDate)
+                        .tz(selectedTimeZone)
+                        .format("ddd, MMMM D, YYYY")}
                 </h3>
             )}
         </div>
