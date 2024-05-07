f390b292c012a2ba416f73438c53bd0a2f52fa17
LizzieDworsky <amydworsky15@gmail.com>
2024-05-07 08:41:54 -0600

    implemented dynamic rendering for user selected date and time on user form

--- app/components/UserForm.tsx
+++ app/components/UserForm.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { DateTimeState } from "../types";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import moment from "moment-timezone";
 
 interface UserFormProps {
     selectedDateTime: DateTimeState;
@@ -8,10 +9,19 @@ interface UserFormProps {
 }
 
 const UserForm = ({ selectedDateTime, setShowDateTimeForm }: UserFormProps) => {
+    console.log(selectedDateTime);
     return (
         <div>
             <div className="date-time-bar">
                 <IoMdArrowRoundBack onClick={() => setShowDateTimeForm(true)} />
+                <div className="calendar-details-container">
+                    <p>{`${selectedDateTime.selectedTime}`}</p>
+                    <p>
+                        {moment(selectedDateTime.selectedDate).format(
+                            "ddd, MMMM D, YYYY"
+                        )}
+                    </p>
+                </div>
             </div>
         </div>
     );
